503b98428c26369c467cb1d75288526b4438a6de
Despar <despargy@ece.auth.gr>
2021-03-24 13:43:31 +0200

    init commit apollo_ros_sdk

---
+++ ap0_com/src/ActionsNSockets.h
@@ -0,0 +1,19 @@
+#ifndef ACTIONSNSOCKETS_H
+#define ACTIONSNSOCKETS_H
+#include "SocketClient.h"
+
+class HandleActionSocket
+{
+  public:
+    int achievedId, tryId;;
+    SocketClient mysocket;
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> *ac;
+    HandleActionSocket();
+    void activeCb();
+    void doneCb(const actionlib::SimpleClientGoalState& state,
+                const move_base_msgs::MoveBaseResultConstPtr& result);
+    void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback);
+    bool actionTo( int stop_id );
+
+};
+#endif
